290605b7fe4675aec79683275678dd0824238a9d
Harsh Vardhan <openroad@chez-vardhan.com>
2023-05-23 07:49:21 -0700

    enhancements etc
    Signed-off-by: Harsh Vardhan <openroad@chez-vardhan.com>

--- config/train_austen.py
+++ config/train_austen.py
@@ -2,7 +2,7 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-austen'
-eval_interval = 250 # keep frequent because we'll overfit
+eval_interval = 500 # keep frequent because we'll overfit
 eval_iters = 2000
 log_interval = 50 # don't print too too often
 
@@ -19,14 +19,16 @@ batch_size = 64
 block_size = 512 # context of up to 256 previous characters
 
 # baby GPT model :)
-# 8/8/512 uses 8G GPU RAM and 25 million parameters
-n_layer = 8
-n_head = 8
-n_embd = 512
+#  8/ 8/512 uses  8G GPU RAM and 25 million parameters
+#  9/ 9/576 uses 10G GPU RAM and 35 million parameters  >40m to "compile"
+# 10/10/640 uses XXG GPU RAM and XX million parameters  not tried yet
+n_layer = 10
+n_head = 10
+n_embd = 640
 dropout = 0.2
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
+learning_rate = 3e-3 # with baby networks can afford to go a bit higher
+max_iters = 45000
 lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
